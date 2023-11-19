a2b07a9192b9b3ab28f84035ee0099e0453c1f68
Md Ariful Islam <arifcse209@gmail.com>
2023-11-19 17:49:30 +0600

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { RiProductHuntLine } from "react-icons/ri";
+import { MdOutlineInventory2 } from "react-icons/md";
 import { Link } from "react-router-dom";
 import "./Home.scss";
 import heroImg from "../../assets/inv-img.png";
@@ -10,7 +10,7 @@ const Home = () => {
     <div className="home">
       <nav className="container --flex-between ">
         <div className="logo">
-          <RiProductHuntLine size={35} />
+          <MdOutlineInventory2 size={35} />
         </div>
 
         <ul className="home-links">
